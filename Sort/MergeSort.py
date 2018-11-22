6160725734c7f78198074304ff3e8556f4a2b4ea
HelloFranker <jianwu925@qq.com>
2018-11-22 15:27:57 +0800

    update sort_alg

---
+++ Sort/MergeSort.py
@@ -0,0 +1,40 @@
+# 归并排序，传入一个list，对list元素进行排序
+def MergeSort(alist):
+     print("Splitting ",alist)
+     if len(alist)>1:
+         mid = len(alist)//2#精确除法，取小于等于结果的最大整数，相当于对结果进行向下取整
+         lefthalf = alist[:mid]
+         righthalf = alist[mid:]
+
+         MergeSort(lefthalf)#递归调用左半部分
+         MergeSort(righthalf)#递归调用右半部分
+ 
+         #合并过程
+         i=0
+         j=0
+         k=0
+         while i < len(lefthalf) and j < len(righthalf):
+             if lefthalf[i] < righthalf[j]:
+                 alist[k]=lefthalf[i]
+                 i=i+1
+             else:
+                 alist[k]=righthalf[j]
+                 j=j+1
+            k=k+1
+ 
+        while i < len(lefthalf):
+             alist[k]=lefthalf[i]
+             i=i+1
+             k=k+1
+ 
+         while j < len(righthalf):
+             alist[k]=righthalf[j]
+             j=j+1
+             k=k+1
+     print("Merging ",alist)
+     # 归并排序算法时间复杂度：n*lgn,空间复杂度:n,相同元素保持顺序不变性
+     
+  if __name__=='__main__':
+     listA=[1,5,7,3,4,6,7,8,9,9,15,10,4]
+     alist = [54,26,93,17,77,31,44,55,20]
+     MergeSort(alist)
